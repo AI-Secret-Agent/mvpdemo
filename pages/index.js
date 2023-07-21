3e5657f89533b117922322716e529d31ba6e2985
Shawn Mason <132690496+aisecretagent@users.noreply.github.com>
2023-07-21 18:35:07 -0400

    jsx?

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,41 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>AI Secret Agent Demo</title>
+        <title>Home Page</title>
       </Head>
-	  <body>
-      <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpTo33F0s&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
-<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpTo33F0s&#x2F;view?utm_content=DAFpTo33F0s&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">AI SECRET AGENT DEMO</a> by Shawn Mason
-    </div>
+      <div style={{
+        position: 'relative', 
+        width: '100%', 
+        height: '0', 
+        paddingTop: '56.2225%',
+        paddingBottom: '0', 
+        boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', 
+        marginTop: '1.6em', 
+        marginBottom: '0.9em', 
+        overflow: 'hidden',
+        borderRadius: '8px', 
+        willChange: 'transform'
+      }}>
+        <iframe 
+          loading="lazy" 
+          style={{
+            position: 'absolute', 
+            width: '100%', 
+            height: '100%', 
+            top: '0', 
+            left: '0', 
+            border: 'none', 
+            padding: '0',
+            margin: '0'
+          }}
+          src="https://www.canva.com/design/DAFpTo33F0s/view?embed" 
+          allowFullScreen
+        >
+        </iframe>
+      </div>
+      <a href="https://www.canva.com/design/DAFpTo33F0s/view?utm_content=DAFpTo33F0s&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Copy of AI SECRET AGENT DEMO</a> by Shawn Mason
+    </>
   );
 }
