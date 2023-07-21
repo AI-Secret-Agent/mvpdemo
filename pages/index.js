494769bbb2653888e8e17ab00066ce0f79662872
Shawn Mason <132690496+aisecretagent@users.noreply.github.com>
2023-07-21 18:28:50 -0400

    embedd

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,17 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Your Website Title</title>
+        <title>AI Secret Agent Demo</title>
       </Head>
-      <iframe 
-        src="https://aisademo.my.canva.site/" 
-        style={{position:"absolute", top:"0", left:"0", bottom:"0", right:"0", width:"100%", height:"100%", border:"none", margin:"0", padding:"0", overflow:"hidden", zIndex:"999999"}}
-        allowFullScreen>
-      </iframe>
+	  <body>
+      <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpTo33F0s&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpTo33F0s&#x2F;view?utm_content=DAFpTo33F0s&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">AI SECRET AGENT DEMO</a> by Shawn Mason
     </div>
   );
 }
